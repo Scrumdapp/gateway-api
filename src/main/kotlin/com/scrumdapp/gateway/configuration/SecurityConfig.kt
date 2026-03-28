e58c7aab1491f8bc9cb1fe4c12ea9aedc8136bf5
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-03-28 18:28:35 +0100

    fixed redirect

--- src/main/kotlin/com/scrumdapp/gateway/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/scrumdapp/gateway/configuration/SecurityConfig.kt
@@ -2,6 +2,8 @@ package com.scrumdapp.gateway.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
@@ -13,11 +15,14 @@ class SecurityConfig {
     fun securityFilterChain(http: HttpSecurity, successHandler: AuthenticationSuccessHandler): SecurityFilterChain {
         http
             .csrf { it.disable() }
-            .authorizeHttpRequests { auth -> auth
-                .requestMatchers("/login/**", "/oath2/**").permitAll()
-                .anyRequest().authenticated()
+            .authorizeHttpRequests {
+                it.requestMatchers("/login/**", "/oath2/**").permitAll()
+                it.anyRequest().authenticated()
             }
-            .oauth2Login { oauth2 -> oauth2.successHandler(successHandler)}
+            .oauth2Login { oauth2 -> oauth2
+                .loginPage("/oauth2/authorization/discord")
+                .successHandler(successHandler
+            )}
             .logout {logout -> logout.logoutSuccessUrl("/")}
 
         return http.build()
