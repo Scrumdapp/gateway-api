9117c41039c3da3b03c1e703bc9b0b11837a74a9
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-09-24 13:15:17 +0200

    added invalidation endpoint

--- src/main/kotlin/com/scrumdapp/gateway/passports/PassportController.kt
+++ src/main/kotlin/com/scrumdapp/gateway/passports/PassportController.kt
@@ -1,4 +1,22 @@
 package com.scrumdapp.gateway.passports
 
-class PassportController {
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.SessionAttribute
+
+@RestController
+@RequestMapping("/api/passports")
+class PassportController(
+    val passportService: PassportService
+) {
+
+    @GetMapping("/@me/invalidate")
+    fun invalidOwnPassport(
+        @SessionAttribute userId: Long,
+    ): ResponseEntity<Void> {
+        passportService.invalidatePassport(userId)
+        return ResponseEntity.ok().build()
+    }
 }
